529b2cae12f828439c2d965258072b140a5be018
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-06-10 22:20:50 -0400

    build: Agregado perfiles Dev y Prod en pom.xml

--- taskmaster/src/main/java/com/grupo3/taskmaster/App.java
+++ taskmaster/src/main/java/com/grupo3/taskmaster/App.java
@@ -10,6 +10,7 @@ public class App {
     public static ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
+        System.out.println("Ambiente: " + System.getProperty("env.name"));
         System.out.println("Bienvenido a TaskMaster!");
         addTask("Estudiar Maven");
         addTask("Leer sobre CI/CD");
